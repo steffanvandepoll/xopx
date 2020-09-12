156e7cc10a2decf545709743e60e6a3685d18de6
Steffan van de Poll <steffanvandepoll@gmail.com>
2020-09-12 15:07:12 +0200

    add title and border

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="game">
-    <div id="actions">
-      <button class="skip" :class="{disabled: turn === 0}" v-on:click="onSkipButtonClicked()">Skip Turn</button>
-      <div id="dices">
-        <div class="numbers"> 
-          <Dice type="number" :id="1" />
-          <Dice type="number" :id="2" />
-          <Dice type="number" :id="3" />
+    <img class="hamburger-icon" src="../assets/hamburger.png">
+    <img class="online-icon" src="../assets/online.png">
+    <h1>KEER OP KEER</h1>
+    <div class="game-container">
+      <div id="actions">
+        <button class="skip" :class="{disabled: turn === 0}" v-on:click="onSkipButtonClicked()">Skip Turn</button>
+        <div id="dices">
+          <div class="numbers"> 
+            <Dice type="number" :id="1" />
+            <Dice type="number" :id="2" />
+            <Dice type="number" :id="3" />
+          </div>
+          <div class="colors">
+            <Dice type="color" :id="4"/>
+            <Dice type="color" :id="5"/>
+            <Dice type="color" :id="6"/>
+          </div>
         </div>
-        <div class="colors">
-          <Dice type="color" :id="4"/>
-          <Dice type="color" :id="5"/>
-          <Dice type="color" :id="6"/>
-        </div>
+        <button class="roll" :class="{disabled: !nextTurnIsPossible}" v-on:click="onRollButtonClicked()">{{turn === 0 ? 'Start' : turn === 30 ? 'Finish' : 'Next turn'}}</button>
       </div>
-      <button class="roll" :class="{disabled: !nextTurnIsPossible}" v-on:click="onRollButtonClicked()">{{turn === 0 ? 'Start' : turn === 30 ? 'Finish' : 'Next turn'}}</button>
+      <div class="sheet-container">
+        <TurnCounter />
+        <Sheet />
+        <Jokers />
+      </div>
+      <Score />
+      <Finish v-if="isFinished"/>
     </div>
-    <div class="sheet-container">
-      <TurnCounter />
-      <Sheet />
-      <Jokers />
-    </div>
-    <Score />
-    <Finish v-if="isFinished"/>
   </div>
 
 </template>
@@ -69,6 +74,33 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #game{
+  position: relative;
+  padding:20px 40px 40px 40px;
+  border: solid 4px white;
+}
+
+h1{
+  color: white;
+  font-size: 71px;
+  margin:0 0 10px 0;
+}
+
+.hamburger-icon, .online-icon{
+  position: absolute;
+}
+
+.hamburger-icon{
+  width:80px;
+  height: 80px;
+  left:-55px;
+}
+
+.online-icon{
+  width:80px;
+  right:-80px;
+}
+
+.game-container{
   display: flex;
 }
 .sheet-container{
